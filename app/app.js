60c54fce65a95f7330052bfadb26105035e9b898
davidmottet <mottet.tlse@gmail.com>
2017-09-30 01:38:23 +0200

    Indent line in app.js

--- app/app.js
+++ app/app.js
@@ -1,8 +1,8 @@
 var http = require('http');
 
 var app = http.createServer(function(req, res) {
-    res.writeHead(200);
-    res.end('Salut tout le monde !');
+	res.writeHead(200);
+	res.end('Salut tout le monde !');
 });
 app.listen(80);
 
