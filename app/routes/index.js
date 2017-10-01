5df7f4b23fbec0f39bfba9a61c78d790703b6f06
davidmottet <mottet.tlse@gmail.com>
2017-10-01 23:59:26 +0200

    introduce partials with wrapper

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function (_req, _res) {
-	_res.render('index', { title: 'App' });
+	_res.render('container', {
+		title: 'App',
+		partials: {content: 'index'}
+	});
 })
 
 module.exports = router;
